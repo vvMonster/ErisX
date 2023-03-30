14b5da76a6e834399d85881aeb1df0b237b3e40a
vvMonster <707686846@qq.com>
2023-03-30 16:52:12 +0800

    commit 2
    commit 2

--- MyTaskContract.sol
+++ MyTaskContract.sol
@@ -2,8 +2,9 @@
 pragma solidity ^0.8.0;
 
 import "./ReputationContract.sol";
+import "./StakingContract.sol";
 
-contract MyTaskContract {
+contract TaskContract {
     struct Task {
         uint256 id;
         address publisher;
@@ -15,17 +16,20 @@ contract MyTaskContract {
     }
     Task[] public tasks;
     ReputationContract reputationContract;
+    StakingContract stakingContract;
 
     event TaskCreated(address indexed publisher, uint256 indexed taskId);
     event TaskCompleted(address indexed receiver, uint256 indexed taskId);
 
-    constructor(ReputationContract _reputationContract) {
+    constructor(ReputationContract _reputationContract, StakingContract _stakingContract) {
         reputationContract = _reputationContract;
+        stakingContract = _stakingContract;
     }
 
     function createTask(string memory _title, string memory _description, uint256 _reward) external {
         require(_reward > 0, "Reward must be greater than zero");
 
+        stakingContract.stake(_reward, msg.sender);
         uint256 taskId = tasks.length;
         tasks.push(Task(taskId, msg.sender, address(0), _title, _description, _reward, false));
 
@@ -39,6 +43,7 @@ contract MyTaskContract {
         require(task.receiver == msg.sender, "Only task receiver can complete the task");
 
         task.completed = true;
+        stakingContract.requestWithdrawal(task.publisher, msg.sender);
 
         emit TaskCompleted(msg.sender, _taskId);
 
@@ -50,9 +55,15 @@ contract MyTaskContract {
         Task storage task = tasks[_taskId];
         require(!task.completed, "Task is already completed");
         require(task.receiver == address(0), "Task is already assigned");
-
+        
         reputationContract.addUser(_receiver); 
         task.receiver = _receiver;
+    }
+    
+    function acceptanceTask(uint256 _taskId, address addr) external {
+        Task storage task = tasks[_taskId];
+        require(msg.sender == task.publisher, "Invalid task addr");
+        stakingContract.chooseRecipient(task.publisher ,addr);
     }
 
     function getTask(uint256 _taskId) external view returns (uint256, address, string memory, string memory, uint256, bool) {
